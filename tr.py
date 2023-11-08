c7cc93041a911e906d072d22ed68d2e01e008666
doresolla <131467212+doresolla@users.noreply.github.com>
2023-11-08 13:40:35 +0800

    Диаграмма состояний генерируется

--- tr.py
+++ tr.py
@@ -1,6 +1,9 @@
 import random
 import matplotlib.pyplot as plt
 import networkx as nx
+import pygraphviz as pgv
+from networkx.drawing.nx_agraph import to_agraph
+import my_networkx as my_nx
 from anytree import Node, RenderTree
 
 
@@ -79,9 +82,9 @@ class translator:
                 return False
             while any(elem in word for elem in self.VN):
                 for index in range(len(word)):
-                    #if self.endings.count(word[0]) > 1 or
+                    # if self.endings.count(word[0]) > 1 or
                     try:
-                        word = self.fold(self,  word)
+                        word = self.fold(self, word)
                     except:
                         print("\nЦепочка не принадлежит грамматике")
                         return False
@@ -93,8 +96,6 @@ class translator:
                 print("\nЦепочка не принадлежит грамматике")
                 return False
 
-
-
     def GetParentAndChidren(self, word):
         parent_node = Node(word)
         vars = self.GetListOfKeys(self, word)
@@ -105,12 +106,11 @@ class translator:
                 # варианты (дети текущего узла)
                 word = self.fold(self, word, vars[i])
                 word = vars[i] + word[len(vars[i]):]
-                Node(word, parent = parent_node)
+                Node(word, parent=parent_node)
                 while 'S' not in parent_node.leaves:
                     self.GetParentAndChidren(self, word)
-      #  print(RenderTree(parent_node))
+        #  print(RenderTree(parent_node))
         return parent_node
-
 
     def GetKeyByValue(value):
         for key in translator.Rules.keys():
@@ -128,7 +128,7 @@ class translator:
                 vars.append(key)
         return vars
 
-    def fold(self, word, replace = None):
+    def fold(self, word, replace=None):
         if replace != None:
             begin = translator.Rules[replace]
             for val in begin:
@@ -142,68 +142,93 @@ class translator:
                 word = translator.GetKeyByValue(word[0]) + word[1:]
             return word
 
-
     def DrawGraph(self):
-        G = nx.DiGraph(directed = False)
+        G = nx.DiGraph(directed=True)
         G.add_node('H')
         G.add_nodes_from(self.VT)
-        print(self.Rules)
-        loc = nx.spring_layout(G)
-        labels_edges = {}
 
+        labels_edges = {}
+        # добавление ребер и меток к ним
         for term in self.Rules.keys():
             for value in self.Rules[term]:
                 if len(value) == 1:
-                    #если конечный символ (a,b)
-                   if value in self.VN:
-                       edge = ('H', term)
-                       if labels_edges.get(edge) != None:
-                          labels_edges[edge] = labels_edges[edge] + ',' + value
-                       else:
-                          labels_edges[edge] = value
-                       G.add_edge('H', term)
-                    #если терминальный символ
-                   else:
-                        #метка для такого ребра не нужна
-                       G.add_edge(value, term)
-                   #G.add_edge(value, term)
+                    # если конечный символ (a,b)
+                    if value in self.VN:
+                        edge = ('H', term)
+                        if labels_edges.get(edge) != None:
+                            labels_edges[edge] = labels_edges[edge] + ',' + value
+                        else:
+                            labels_edges[edge] = value
+                        G.add_edge('H', term)
+                    # если терминальный символ
+                    else:
+                        # метка для такого ребра не нужна
+                        G.add_edge(value, term)
+                    # G.add_edge(value, term)
                 elif len(value) == 2:
                     edge = (value[0], term)
                     if labels_edges.get(edge) != None:
-                        labels_edges[edge] = labels_edges[edge] +','+ value[1]
+                        labels_edges[edge] = labels_edges[edge] + ',' + value[1]
                     else:
                         labels_edges[edge] = value[1]
                     G.add_edge(value[0], term)
-        print(labels_edges)
 
-        plt.figure()
-        nx.draw_networkx_edge_labels(G, loc, edge_labels=labels_edges, font_color="red")
-       # nx.draw(G, with_labels=True, connectionstyle='arc3, rad = 0.1')
-        nx.draw(G, with_labels=True)
-        #plt.axis('off')
-        plt.show()
+        for edge in G.edges():
+            if edge[0] == edge[1]:
+                self.LoopGraph(self,G.edges(), labels_edges)
+                return
+        curved_edges = [edge for edge in G.edges() if edge[::-1] in G.edges() and edge[::-1] != edge]
+        straight_edges = list(set(G.edges() - set(curved_edges)))
+        circle_edges = [edge for edge in G.edges() if edge == edge[::-1]]
 
+        pos = nx.spring_layout(G, seed=5)
+        fig, ax = plt.subplots()
 
-class graph:
-    def DrawGraph(self):
+        nx.draw_networkx_nodes(G, pos, ax=ax)
+        nx.draw_networkx_labels(G, pos, ax=ax)
+        labels_curved = {edge: labels_edges[edge] for edge in labels_edges.keys() if edge in curved_edges}
+        labels_straight = {edge: labels_edges[edge] for edge in labels_edges.keys() if edge in straight_edges}
 
-        edges = [['A', 'B'], ['B', 'C'], ['B', 'D']]
-        G = nx.DiGraph()
-        G.add_edges_from(edges)
-        pos = nx.spring_layout(G)
-        plt.figure()
-        nx.draw(
-            G, pos, edge_color='black', width=1, linewidths=1,
-            node_size=500, node_color='pink', alpha=0.9,
-            labels={node: node for node in G.nodes()}
-        )
-        nx.draw_networkx_edge_labels(
-            G, pos,
-            edge_labels={('A', 'B'): 'AB',
-                         ('B', 'C'): 'BC',
-                         ('B', 'D'): 'BD'},
-            font_color='red'
-        )
+        # прямые ребра
+        nx.draw_networkx_edges(G, pos, ax=ax, edgelist=straight_edges)
+        # загнутые ребра
+        arc_rad = 0.25
+        nx.draw_networkx_edges(G, pos, ax=ax, edgelist=curved_edges, connectionstyle=f'arc3, rad = {arc_rad}',
+                               arrows=True)
+
+        # метки для загнутых ребер
+        my_nx.my_draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=labels_curved, rotate=False, rad=arc_rad)
+        # метки для прямых ребер
+        nx.draw_networkx_edge_labels(G, pos, ax=ax, edge_labels=labels_straight, rotate=False)
+
         plt.axis('off')
         plt.show()
 
+        fig.savefig("диаграмма состояний.png", bbox_inches='tight', pad_inches=0)
+
+    def LoopGraph(self, edges, labels_edges):
+        G = nx.MultiDiGraph()
+
+        # add edges
+        for edge in edges:
+            if edge[0] == edge[1]:
+                G.add_edge(edge[0], edge[1], color='red')
+            else:
+                G.add_edge(edge[0], edge[1])
+
+        #print(G.edges(data=True))
+        G.graph['edge'] = {'arrowsize': '0.6', 'splines': 'curved'}
+        G.graph['graph'] = {'scale': '3'}
+
+        A = to_agraph(G)
+
+        A.graph_attr['strict'] = True
+        A.graph_attr['rankdir']='LR'
+        A.layout('dot')
+
+        # set edge labels
+        for pair in labels_edges:
+            edge = A.get_edge(pair[0], pair[1])
+            edge.attr['label'] = str(labels_edges[pair]) + "  "
+
+        A.draw('диаграмма состояний (петля).png')
